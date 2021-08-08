2c3ad0a892a5bb1c491a4adaf7bf63af815391ce
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-08-08 14:39:49 +0200

    Add Material-Ui to MainLayout and PageNav

--- src/App.js
+++ src/App.js
@@ -12,25 +12,43 @@ import BookingInfo from './components/views/BookingInfo/BookingInfo';
 import NewBooking from './components/views/NewBooking/NewBooking';
 import EventInfo from './components/views/EventInfo/EventInfo';
 import NewEvent from './components/views/NewEvent/NewEvent';
+import { StylesProvider } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2B4C6F',
+    },
+    // secondary: {
+    //   main: '#11cb5f',
+    // },
+  },
+});
 
 function App() {
   return (
     <BrowserRouter basename={'/panel'}>
-      <MainLayout>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard}/>
-          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login}/>
-          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen}/>
-          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables}/>
-          <Route exact path={process.env.PUBLIC_URL + '/tables/booking/new'} component={NewBooking}/>
-          <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={BookingInfo}/>
-          <Route exact path={process.env.PUBLIC_URL + '/tables/events/new'} component={NewEvent}/>
-          <Route exact path={process.env.PUBLIC_URL + '/tables/events/:id'} component={EventInfo}/>
-          <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter}/>
-          <Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component={NewOrder}/>
-          <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={OrderInfo}/>
-        </Switch>
-      </MainLayout>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <MainLayout>
+            <Switch>
+              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard}/>
+              <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login}/>
+              <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen}/>
+              <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables}/>
+              <Route exact path={process.env.PUBLIC_URL + '/tables/booking/new'} component={NewBooking}/>
+              <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={BookingInfo}/>
+              <Route exact path={process.env.PUBLIC_URL + '/tables/events/new'} component={NewEvent}/>
+              <Route exact path={process.env.PUBLIC_URL + '/tables/events/:id'} component={EventInfo}/>
+              <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter}/>
+              <Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component={NewOrder}/>
+              <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={OrderInfo}/>
+            </Switch>
+          </MainLayout>
+        </ThemeProvider>
+      </StylesProvider>
     </BrowserRouter>
   );
 }
